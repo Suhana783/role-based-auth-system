ebb8ca8471104b974df8bfc855d86e2939a8e4ac
Suhana783 <suhanachauhan133@gmail.com>
2026-01-07 22:11:07 +0530

    feat(auth): initialize authentication routes

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,5 +1,13 @@
-import { Router } from "express";
+import {Router} from "express";
 
 const router = Router();
+
+router.post("/register", (req, res) => {
+    res.jsonp({message: "REgister route working"});
+});
+
+router.post("/login", (req, res) => {
+    res.json({message : "Login route working"});
+});
 
 export default router;
